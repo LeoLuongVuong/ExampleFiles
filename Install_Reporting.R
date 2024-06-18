e760b2df2100640b2c3df165bf8881ebf9a355b5
My-Luong Vuong <myluong.vuong@student.kuleuven.be>
2024-06-18 09:52:53 +0200

    update project name

---
+++ Install_Reporting.R
@@ -0,0 +1,20 @@
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("officedown")
+install.packages("quarto")
+install.packages("gtsummary")
+install.packages("pander")
+
+library(rmarkdown)
+library(knitr)
+library(officedown)
+library(quarto)
+library(gtsummary)
+library(pander)
+library(tinytex)
+
+# https://yihui.org/tinytex/
+# If you don't have a Latex-installation, uncomment the following lines and run:
+
+# install.packages('tinytex')
+# tinytex::install_tinytex()
